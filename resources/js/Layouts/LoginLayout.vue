209df54cb5b5c0497a652f5d3a58b6dbc1560bf8
chotipong deemanop <chotipong.deemanop@gmail.com>
2023-10-06 10:22:46 +0700

    update Login

--- resources/js/Layouts/LoginLayout.vue
+++ resources/js/Layouts/LoginLayout.vue
@@ -7,7 +7,7 @@ import { Link } from "@inertiajs/vue3";
 
 <template>
     <layoutPage>
-        <div class="min-h-screen bg-gray-200 text-gray-900 flex justify-center">
+        <div class=" min-h-screen bg-gray-200 text-gray-900 flex justify-center">
             <div class="max-w-screen-md m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
                 <slot />
             </div>
